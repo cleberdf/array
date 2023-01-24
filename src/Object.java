60d6f535b24a8aeb516301e9c4d2e95bb85808a7
bill <cleber.flauzino@hotmail.com>
2023-01-23 23:28:08 -0300

    first commit

---
+++ src/Object.java
@@ -0,0 +1,12 @@
+// criação de um array
+public class Object {
+
+  public static void main(String[] args) {
+
+    String[] cars = { "Volvo", " BMW", "Ford", "Mazda" };
+    for (String i : cars) {
+      System.out.println(i);
+    }
+  }
+
+}
